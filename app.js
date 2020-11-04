7036bec964f60a0f16050cbdab667bf94dd6de79
Regina01122020 <73094130+Regina01122020@users.noreply.github.com>
2020-11-04 16:26:48 +1000

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 var express = require('express');
+var storage = require('node-persist');
 var ejs = require('ejs');
 var path = require('path');
-var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
-const port = 3000
 
+const port = 3000
+const app = express(); 
 
 mongoose.connect('mongodb://localhost/BackendProjectDB', { useNewUrlParser: true, useUnifiedTopology: true },);
-
-var db = mongoose.connection;
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'database connection error:'));
 db.once('open', () => {
 });
@@ -25,12 +25,33 @@ app.use(session({
   })
 }));
 
+
+// Holds the current views
+var counter = 0;
+// Registers an event triggered on HTTP GET /visit
+app.get("/visit", (req, res) => {
+  counter++;
+  // Saves counter into the store and send response AFTER the store has been saved
+  storage.setItem("counter", counter).then(() => {
+      res.json(counter);
+  });
+});
+// Inits permanent storage and reads the saved counter
+storage.init().then(() => storage.getItem("counter")).then((value) => {
+  // Checks if value read is valid, otherwise set it to 0
+  if (value > 0) {
+      counter = value;
+  } else {
+      counter = 0;
+  }
+}) 
+
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(express.static(__dirname + '/views'));
 
 var index = require('./routes/index');
@@ -50,8 +71,7 @@ app.use((err, req, res, next) => {
   res.send(err.message);
 });
 
-
 // listen on port 3000
 app.listen(port, () => {
-  console.log(`Express app listening at http://localhost:${port}`);
-});
+  console.log(`Express app listening at http://localhost:${port}`)
+}) 
